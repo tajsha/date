3eac86cb72e068d8a1a6e0253b32588c80f9da25
pwz2k <pwz2k@hotmail.com>
2013-08-27 09:38:42 -0400

    working on search

--- app/indices/user_index.rb
+++ app/indices/user_index.rb
@@ -1,11 +1,9 @@
 ThinkingSphinx::Index.define :user, :with => :active_record do
   # fields
   indexes name, :as => :user, :sortable => true
-  indexes [ethnicity, religion, about_me, sexuality, children, user_smoke, user_drink, age, gender]
-  indexes ethnicity
-  indexes religion
-  indexes age
-  indexes gender
+  indexes religion, about_me, sexuality, children, user_smoke, user_drink, age, gender
+  indexes ethnicity.name, :as => :ethnicity
+  indexes gender.name, :as => :gender
 # attributes
-  has id, created_at, updated_at
+  has created_at, updated_at
 end
