7431adeb3ab1741e645b86f143818ded853506fd
pwz2k <pwz2k@hotmail.com>
2014-05-31 12:29:04 -0400

    working on q&a to mailbox

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,47 +1,36 @@
 class QuestionsController < ApplicationController
   respond_to :js, :html
-  
-  def index
-    @questions = Question.where(recipient_id: params[:id]).page(params[:page]).per_page(3) if params[recipient_id].present?
-    @questions = Question.all
+
+
+  def show
+    @question = Question.find(params[:id])
+    @questions = Question.order("created_at DESC")
     respond_with(@questions)
-end
-
-def show
-  @question = Question.find(params[:id])
-  @questions = Question.order("created_at DESC")
-  respond_with(@questions)
-end
-
-def new
-  @question = Question.new
-  respond_with(@question)
-end
-
-def create
-@conversation = Conversation.find(params[:conversation_id])
-    @question = @conversation.questions.build(params[:question])
-    if @question.save
-      #scoping to the current user is the right thing to do here
-      @message = current_user.messages.new(:subject => "You have a question from #{@question.sender_id}",
-                             #Original code :sender_id
-                             :notification_id => @question.sender_id,
-                             #Original code :recipient_id
-                             :receiver_id => @question.recipient_id,
-                             :body => @question.question)
-
-      @question.message = @message
-      @question.save
-      redirect_to questions_path, notice: 'Your question was saved successfully. Thanks!'
-    else
-      render :new, alert: 'Sorry. There was a problem saving your question.'
-    end
   end
 
+  def new
+    @question = Question.new
+    respond_with(@question)
+  end
 
-  def update
-    @question = Question.find(params[:id])
-    @question.update_attributes(:answer => params[:question][:answer])
-    redirect_to user_messages_path(current_user, :mailbox => "inbox")
-end
-end
+  def create
+@question = Question.new(params[:question])
+      @conversation = Conversation.find(params[:conversaton_id])
+      if @question.save
+        #scoping to the current user is the right thing to do here
+        @message = current_user.messages.new(:subject => "You have a question from #{@question.sender_id}",
+                               #Original code :sender_id
+                               :notification_id => @question.sender_id,
+                               #Original code :recipient_id
+                               :receiver_id => @question.recipient_id,
+                               :conversation_id => @conversation.id,
+                               :body => @question.question)
+
+        @question.message = @message
+        @question.save
+        redirect_to :back, notice: 'Your question was saved successfully. Thanks!'
+      else
+        render :new, alert: 'Sorry. There was a problem saving your question.'
+      end
+    end
+  end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,14 @@
 class Question < ActiveRecord::Base
-  attr_accessible :answer, :question, :sender_id, :recipient_id
+  attr_accessible :answer, :question, :sender_id, :recipient_id, :conversation_id
+
   belongs_to :user
-  
-  belongs_to :sender,
-	:class_name => 'User',
-	:foreign_key => 'sender_id'
-	belongs_to :recipient,
-	:class_name => 'User',
-	:foreign_key => 'recipient_id'
-	
-	belongs_to :message
-	
-end
+
+  belongs_to :sender,:class_name => 'User',:foreign_key => 'sender_id'
+
+  belongs_to :recipient,:class_name => 'User',:foreign_key => 'recipient_id'
+
+  belongs_to :message
+
+  belongs_to :conversation
+
+  end
