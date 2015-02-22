cc11f15f4c1ef3fa6a82703e90d667e6a4abbf5f
shaik <shaik@mahaswami.com>
2015-02-22 19:26:52 +0530

    User zip_code field error.

--- app/indices/user_index.rb
+++ app/indices/user_index.rb
@@ -7,12 +7,11 @@ end
 
 ThinkingSphinx::Index.define :user, :with => :active_record do 
   indexes name, :as => :user, :sortable => true 
-  indexes religion, birthday, about_me, height, career, feet, inches, sexuality, children, user_smoke, user_drink, politics, gender, ethnicity, education, username
+  indexes religion, birthday, about_me, height, career, feet, inches, sexuality, children, user_smoke, user_drink, politics, gender, ethnicity, education, username, zip_code
   has created_at, updated_at 
   has "RADIANS(locations.latitude)",  :as => :latitude,  :type => :float
   has "RADIANS(locations.longitude)", :as => :longitude, :type => :float
   has(:id, :as => :user_id)
-  has(:zip_code, :as => :zip_code, :type => :integer)
   has age
   set_property :wordforms => 'lib/word.txt'
   join location
