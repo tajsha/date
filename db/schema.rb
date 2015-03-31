d8b9f3a38730b7832591503dacd014bbbdfed1c8
pwz2k <pwz2k@hotmail.com>
2015-03-31 18:48:29 -0400

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317173214) do
+ActiveRecord::Schema.define(version: 20150326183821) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -250,6 +250,7 @@ ActiveRecord::Schema.define(version: 20150317173214) do
     t.boolean  "advanced",   default: false
     t.text     "input_text"
     t.integer  "user_id",                    null: false
+    t.string   "sexuality"
   end
 
   create_table "smailer_properties", force: true do |t|
