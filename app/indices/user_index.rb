50fb497a20ac5275417657e7d310a978435213d8
shaik <shaik@mahaswami.com>
2015-04-02 22:30:09 +0530

    random applying the details.

--- app/indices/user_index.rb
+++ app/indices/user_index.rb
@@ -8,7 +8,7 @@ end
 ThinkingSphinx::Index.define :user, :with => :active_record, :delta => true do 
   indexes name, :as => :user, :sortable => true 
   indexes letsgos.content
-  indexes religion, birthday, about_me, height, career, feet, inches, sexuality, children, user_smoke, user_drink, politics, gender, ethnicity, education, username, zip_code
+  indexes religion, about_me, height, career, feet, inches, sexuality, children, user_smoke, user_drink, politics, gender, ethnicity, education
   has created_at, updated_at 
   has "RADIANS(locations.latitude)",  :as => :latitude,  :type => :float
   has "RADIANS(locations.longitude)", :as => :longitude, :type => :float
